3db1b6abfe99437e22eeed9bd6a17d8a05e65358
Matthew Iannucci <mpiannucci@gmail.com>
2016-07-05 11:29:36 -0400

    Add mux dependency, add blank api doc html

--- buoyfinder.go
+++ buoyfinder.go
@@ -3,16 +3,29 @@ package buoyfinder
 import (
 	"html/template"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 var indexTemplate = template.Must(template.New("base.html").Funcs(nil).ParseFiles("templates/base.html", "templates/index.html"))
+var apiDocTemplate = template.Must(template.New("base.html").Funcs(nil).ParseFiles("templates/base.html", "templates/apidoc.html"))
 
 func init() {
-	http.HandleFunc("/", indexHandler)
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", indexHandler)
+	router.HandleFunc("/api", apiDocHandler)
+
+	http.Handle("/", router)
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	if err := indexTemplate.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func apiDocHandler(w http.ResponseWriter, r *http.Request) {
+	if err := apiDocTemplate.Execute(w, nil); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
