e1309b345acbce5b816b763cae7660fa5eb62527
Matthew Iannucci <mpiannucci@gmail.com>
2016-07-28 22:38:49 -0400

    Closest buoy works with postivie and negative. gunna have to refine search for buoy features

--- buoyfinder.go
+++ buoyfinder.go
@@ -1,11 +1,19 @@
 package buoyfinder
 
 import (
+	"encoding/xml"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/mpiannucci/surfnerd"
+	"golang.org/x/net/context"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/urlfetch"
 )
 
 var indexTemplate = template.Must(template.New("base.html").Funcs(nil).ParseFiles("templates/base.html", "templates/index.html"))
@@ -33,12 +41,34 @@ func apiDocHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func buoyHandler(w http.ResponseWriter, r *http.Request) {
+	ctxParent := appengine.NewContext(r)
+	ctx, _ := context.WithTimeout(ctxParent, 20*time.Second)
+	client := urlfetch.Client(ctx)
+
 	vars := mux.Vars(r)
 
 	// Grab the user vars
-	latitude := vars["lat"]
-	longitude := vars["lon"]
-	rawdate := vars["epoch"]
+	latitude, _ := strconv.ParseFloat(vars["lat"], 64)
+	longitude, _ := strconv.ParseFloat(vars["lon"], 64)
+	rawdate, _ := strconv.ParseInt(vars["epoch"], 10, 64)
 
-	fmt.Fprintf(w, "Lat: %v\nLon: %v\nEpoch: %v", latitude, longitude, rawdate)
+	// TODO: Find the closest buoy
+	stationsResp, stationsErr := client.Get(surfnerd.ActiveBuoysURL)
+	if stationsErr != nil {
+		http.Error(w, stationsErr.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer stationsResp.Body.Close()
+
+	stationsContents, _ := ioutil.ReadAll(stationsResp.Body)
+	stations := surfnerd.BuoyStations{}
+	xml.Unmarshal(stationsContents, &stations)
+
+	closestBuoy := stations.FindClosestActiveBuoy(surfnerd.NewLocationForLatLong(latitude, longitude))
+
+	// TODO: Get the closest buoy reading
+
+	// For now just print out the results
+	fmt.Fprintf(w, "Lat: %v\nLon: %v\nDate: %v\n", latitude, 360-longitude, time.Unix(rawdate, 0))
+	fmt.Fprintf(w, "ClosestStation: %v", closestBuoy.StationID)
 }
