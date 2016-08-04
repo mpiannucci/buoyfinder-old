d9f35c88f2329d4da9ddb24995e14f207b3cfb70
Matthew Iannucci <mpiannucci@gmail.com>
2016-08-03 22:28:02 -0400

    Working closest buoy finding and condition finding!!!

--- buoyfinder.go
+++ buoyfinder.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -23,7 +24,7 @@ func init() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", indexHandler)
 	router.HandleFunc("/api", apiDocHandler)
-	router.HandleFunc("/api/{lat}/{lon}/{epoch}", buoyHandler)
+	router.HandleFunc("/api/{lat}/{lon}/{epoch}", closestBuoyHandler)
 
 	http.Handle("/", router)
 }
@@ -40,7 +41,7 @@ func apiDocHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func buoyHandler(w http.ResponseWriter, r *http.Request) {
+func closestBuoyHandler(w http.ResponseWriter, r *http.Request) {
 	ctxParent := appengine.NewContext(r)
 	ctx, _ := context.WithTimeout(ctxParent, 20*time.Second)
 	client := urlfetch.Client(ctx)
@@ -52,7 +53,7 @@ func buoyHandler(w http.ResponseWriter, r *http.Request) {
 	longitude, _ := strconv.ParseFloat(vars["lon"], 64)
 	rawdate, _ := strconv.ParseInt(vars["epoch"], 10, 64)
 
-	// TODO: Find the closest buoy
+	// Find the closest buoy
 	stationsResp, stationsErr := client.Get(surfnerd.ActiveBuoysURL)
 	if stationsErr != nil {
 		http.Error(w, stationsErr.Error(), http.StatusInternalServerError)
@@ -65,10 +66,33 @@ func buoyHandler(w http.ResponseWriter, r *http.Request) {
 	xml.Unmarshal(stationsContents, &stations)
 
 	closestBuoy := stations.FindClosestActiveWaveBuoy(surfnerd.NewLocationForLatLong(latitude, longitude))
+	if closestBuoy == nil {
+		http.Error(w, "Could not find the closest buoy", http.StatusInternalServerError)
+		return
+	}
 
-	// TODO: Get the closest buoy reading
+	// Get the buoy data
+	requestedDate := time.Unix(rawdate, 0)
+	buoyResp, buoyErr := client.Get(closestBuoy.CreateDetailedWaveDataURL())
+	if buoyErr != nil {
+		http.Error(w, buoyErr.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer buoyResp.Body.Close()
+
+	buoyContents, _ := ioutil.ReadAll(buoyResp.Body)
+	rawBuoyData := strings.Fields(string(buoyContents))
+
+	buoyParseError := closestBuoy.ParseRawDetailedWaveData(rawBuoyData, 100000000)
+	if buoyParseError != nil {
+		http.Error(w, buoyParseError.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	closestBuoyData, _ := closestBuoy.FindConditionsForDateAndTime(requestedDate)
 
 	// For now just print out the results
 	fmt.Fprintf(w, "Lat: %v\nLon: %v\nDate: %v\n", latitude, 360-longitude, time.Unix(rawdate, 0))
-	fmt.Fprintf(w, "ClosestStations: %v", closestBuoy.StationID)
+	fmt.Fprintf(w, "ClosestStations: %v\n", closestBuoy.StationID)
+	fmt.Fprintf(w, "Closest Buoy Data:\n\n%v", closestBuoyData)
 }
