0eae1747006a3291587928afa85d47878894f30f
Matthew Iannucci <mpiannucci@gmail.com>
2016-09-22 22:13:04 -0400

    Fix size because hidpi screens

--- buoyfinder.go
+++ buoyfinder.go
@@ -64,6 +64,7 @@ func chartTestHandle(w http.ResponseWriter, r *http.Request) {
 	data := url.Values{}
 	data.Set("content", "options")
 	data.Set("options", "{xAxis: {categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']},series: [{data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]}]};")
+	data.Set("width", "1200")
 	data.Set("type", "image/png")
 	data.Set("constr", "Chart")
 
